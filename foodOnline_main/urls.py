972e6245217ccac6333c36d609ba7a11cadf648b
Zenón Burgos <znburgos@gmail.com>
2022-09-20 14:25:56 -0600

    Plantilla organizada en bloques.

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -1,6 +1,6 @@
 from xml.dom.minidom import Document
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,4 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('accounts/', include('accounts.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
