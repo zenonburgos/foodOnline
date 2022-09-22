4758643437f0678c1dc6cbc96b2eb52faa9f2f99
Zenón Burgos <znburgos@gmail.com>
2022-09-22 13:22:48 -0600

    Customer and vendor registration

--- accounts/models.py
+++ accounts/models.py
@@ -76,6 +76,13 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendedor'
+        elif self.role == 2:
+            user_role = 'Cliente'
+        return user_role
+
 
 class UserProfile(models.Model):
     user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
 
 from accounts.models import User, UserProfile
-from django.contrib import messages
-
 from vendor.forms import VendorForm
-
 from .forms import UserForm
+from accounts.utils import detectUser
 
+# Restrict the vendor from accesing the customer dashboard
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+
+# Restrict the customer from accesing the vendor dashboard
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
 
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'Ya estás registrado en el sistema y actualmente logueado.')
+        return redirect('dashboard')
+    elif request.method == 'POST':
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
@@ -43,7 +60,10 @@ def registerUser(request):
     return render(request, 'accounts/registerUser.html', context)
 
 def registerVendor(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'Ya estás registrado en el sistema y actualmente logueado.')
+        return redirect('dashboard')
+    elif request.method == 'POST':
         #store data and create the user
         form = UserForm(request.POST)
         v_form = VendorForm(request.POST, request.FILES)
@@ -76,3 +96,44 @@ def registerVendor(request):
     }
 
     return render(request, 'accounts/registerVendor.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'Ya estás logueado en el sistema.')
+        return redirect('myAccount')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Ha ingresado correctamente.')
+            return redirect('myAccount')
+        else:
+            messages.error(request, 'Credenciales inválidas.')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request, 'Has cerrado sesión, te esperamos pronto.')
+    return redirect('login')
+
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    return render(request, 'accounts/custDashboard.html')
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    return render(request, 'accounts/vendorDashboard.html')
