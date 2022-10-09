0b9a70d4f458ee2b562333828edb075e80f640c0
Zenón Burgos <znburgos@gmail.com>
2022-10-08 22:15:15 -0600

    order and order details for cust/vendors

--- orders/admin.py
+++ orders/admin.py
@@ -9,7 +9,7 @@ class OrderFoodInline(admin.TabularInline):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['order_number', 'name', 'phone', 'email', 'total', 'payment_method', 'status', 'is_ordered']
+    list_display = ['order_number', 'name', 'phone', 'email', 'total', 'payment_method', 'status', 'order_placed_to', 'is_ordered']
     inlines = [OrderFoodInline]
 
 admin.site.register(Payment)
